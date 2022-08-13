42fbc9a63c4d057eec4adb457a19bc6daacf98a4
Alex Belyaev <t433621@yandex.ru>
2022-08-13 16:04:11 +0300

    Среднее арифметическое каждого столбца

---
+++ Task52/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+double SumElemI = 0;
+double midl = 0;
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+FillArray(array);
+Console.WriteLine("Выводим массив случайных чисел");
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        SumElemI += array[i, j];
+
+        if (i == m - 1)
+        {
+            midl = SumElemI / m;
+            midl = Math.Round(midl, 2);
+
+            SumElemI = 0;
+        }
+    }
+    Console.WriteLine("Среднее арифметическое " + (j + 1) + " столбца = " + midl);
+}
